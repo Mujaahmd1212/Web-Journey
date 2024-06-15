9122266e2278be5e2b78b9db1e5f57e75f137f75
Mujahid Ahmed <mujaicloud1212@gmail.com>
2024-06-16 00:14:11 +0530

    bro code

---
+++ Bro Code/index.js
@@ -0,0 +1,92 @@
+/*//sesion - 01***********
+
+console.log("i like pizza");
+console.log("It's really good");
+
+let firstname = "Mujahid";//Strings
+let age = 21;//Numbers
+let student = true//Booleans
+
+console.log("hello" , firstname);
+console.log("You are" , age , "Years old");
+console.log(student);
+
+document.getElementById("p1").innerHTML = "hello " + firstname;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student; */
+
+
+
+/*//session - 02**************
+
+let students = 20;
+
+ students = students + 2;
+students = students - 2;
+students = students * 2;
+students = students / 2; //
+
+//let extrastudents = students % 3; -- result is (2)
+
+//students *= 2 //shortform
+
+let result = (1 + 2) * (3 + 4);
+
+console.log(result); */
+
+
+/*//session - 03************
+
+//let username = window.prompt("Whats Your Name");
+//console.log(username);
+
+let username
+document.getElementById("mybutton").onclick = function(){
+
+  username = document.getElementById("mytext").value;
+  console.log(username);
+  document.getElementById("myLabel").innerHTML = "hlw " + username;
+} */
+
+
+/*//session - 04*************
+
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+ age += 1;
+
+console.log("Happy Birthday! You are", age, "Years Old");
+
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("pizza");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);*/
+
+/*//session  - 05***********
+const PI = 3.14159
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+//pi = 420.69;
+
+circumference = 2 * PI * radius;
+
+console.log("The circumference is: " , circumference);*/
+
+//session -06***************
+
+
+
